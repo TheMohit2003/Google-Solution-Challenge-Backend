f34b1192beaaf73c0d6190344e303f26fb8e3186
mohit pardeshi <mohitpardeshi729@gmail.com>
2024-02-18 21:45:00 +0530

    added root files

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hey , this is the get api.');
+});
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
-})
+    console.log(`Server is running on port ${port}`);
+});
