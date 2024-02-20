162e7946a67b77ef76bef077fe14fd19441b1ded
mohit pardeshi <mohitpardeshi729@gmail.com>
2024-02-20 12:03:16 +0530

    cors added , sql migration

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const userRoutes = require('./routes/users');
 // const bidsRoutes = require('./routes/bids');
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
---
+++ controllers/bidController.js
@@ -0,0 +1,1 @@
+const { PrismaClient } = require('@prisma/client');
